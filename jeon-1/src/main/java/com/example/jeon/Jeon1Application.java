7539e455a85b5d93d6552510afadf3d4092e7dae
jkj0125 <76513302+jkj0125@users.noreply.github.com>
2021-05-07 20:34:56 +0900

    Add files via upload

---
+++ jeon-1/src/main/java/com/example/jeon/Jeon1Application.java
@@ -0,0 +1,13 @@
+package com.example.jeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jeon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jeon1Application.class, args);
+	}
+
+}
